2af3838b4142edb1cf085a55c8c19a975c77161f
Hannah Hall <hannahkhall13@gmail.com>
2016-06-13 15:56:46 -0500

    red pieces move in one direction, reset button

--- app.js
+++ app.js
@@ -43,7 +43,6 @@ angular.module('app', [])
 		// }
 		return {
 			squares () {
-				console.log(squares);
 				return squares;
 			},
 			pieces () {
@@ -52,10 +51,13 @@ angular.module('app', [])
 		}
 	})
 
-	.controller('GameCtrl', function (BoardFact, $timeout) {
+	.controller('GameCtrl', function (BoardFact, $timeout, $animate) {
 		const game = this;
+		var currentPiece;
+		var choice1;
+		var choice2;
 		game.heading = "Checkers";
-		firebase.database().ref('board/').on('value', (snap) => {
+		firebase.database().ref('board/').once('value').then((snap) => {
 			game.board = snap.val();
 			$timeout();
 		})
@@ -63,17 +65,108 @@ angular.module('app', [])
 			game.pieces = snap.val();
 			$timeout();
 		})
-		// game.board = BoardFact.squares();
-		// game.pieces = BoardFact.pieces();
 		game.chckBrd = (x, y) => {
 			var oddX = x % 2;
 			var oddY = y % 2;
 			return (oddX ^ oddY);
 		}
-		game.choosePiece = (x,y) => {
 
+		function removeSelected () {
+			currentPiece = null;
+			choice1 = null;
+			choice2 = null;
+			$('.selected').removeClass('selected');
 		}
 
+
+		game.choosePiece = (e, piece, id) => {
+			var currentElement = e.currentTarget;
+			var currentSquare;
+			var possibleSquare;
+			currentPiece = piece;
+			currentPiece.id = id;
+			$(currentElement).addClass('selected');
+			if(piece.color === 'red') {
+
+				function Move1 (x,y, index) {
+					this.index = index + 7;
+					this.x = x + 1;
+					this.y = y + 1;
+				}
+				function Move2 (x,y, index) {
+					this.index = index + 9;
+					this.x = x - 1;
+					this.y = y + 1;
+				}
+
+				for (var key in game.board) {
+					if(piece.x === game.board[key].y && piece.y === game.board[key].x) {
+						currentSquare = game.board[key];
+						var move1 = new Move1(currentSquare.x, currentSquare.y, currentSquare.index);
+						var move2 = new Move2(currentSquare.x, currentSquare.y, currentSquare.index);
+					}
+				}
+				for (var key in game.board) {
+					if (move1.index === game.board[key].index) { //&& is empty
+						$(`#${key}`).addClass('selected');
+						choice1 = game.board[key];
+					} else if (move2.index === game.board[key].index) { //&& is empty
+						$(`#${key}`).addClass('selected');
+						choice2 = game.board[key];
+					}
+				}
+
+
+			} else {
+				console.log('white')
+			}
+
+		}
+		game.chooseSquare = (square) => {
+			if(square === choice1 || square === choice2) {
+				console.log(currentPiece);
+				var newTop = (square.x * 70) + 'px';
+				var newLeft = (square.y * 70) + 'px';
+				firebase.database().ref(`/pieces/${currentPiece.id}`).update({
+					top: newTop,
+					left: newLeft,
+					y: square.x,
+					x: square.y
+				});
+				removeSelected();
+
+			}
+		}
+
+		game.reset = () => {
+			firebase.database().ref('/pieces/').remove();
+			var pieceCount = 16;
+			for(var i=0; i<pieceCount; i++) {
+				if (i < pieceCount/2) {
+					// player 1
+					var y = Math.floor(i / 4);
+					var x = (i % 4) * 2 + (1 - y%2);
+					firebase.database().ref('/pieces/').push({
+						'color': 'red',
+						'top':  (y * 70)+'px',
+						'left': (x * 70)+'px',
+						'x': x,
+						'y': y
+					});
+				}	else {
+					// player 2
+					var y = Math.floor(i/4) + 4;
+					var x = (i % 4) * 2 + (1-y%2);
+					firebase.database().ref('pieces/').push({
+						'color': 'white',
+						'top':  (y * 70)+'px',
+						'left': (x * 70)+'px',
+						'x': x,
+						'y': y
+					});
+				}
+			}
+		}
 	})
 
 
